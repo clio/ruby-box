a3f34524f78572f6067ba367127bf82f3d1fe734
Dylan Runkel <dylan.runkel@gmail.com>
2015-12-22 15:01:32 -0800

    move-isolation add spec for move isolation

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -28,4 +28,22 @@ describe RubyBox::Item do
       banana.instance_of?(RubyBox::Item).should == true
     end
   end
+
+  describe '#move_to' do
+    let(:source_folder) { RubyBox::Item.new(@session, {'id' => 1, 'etag' => '0' }) }
+    let(:destination)   { RubyBox::Item.new(@session, {'id' => 100, 'etag' => '0' }) }
+    let(:last_request) { JSON.parse(@request.body) }
+    let(:last_uri) { @uri.to_s }
+
+    before(:each) do
+      @session.stub(:request).with do |uri, request, _, _|
+        @uri, @request = uri, request
+      end
+    end
+
+    it 'does not include name in the request' do
+      source_folder.move_to destination.id
+      last_request.should_not have_key 'name'
+    end
+  end
 end
